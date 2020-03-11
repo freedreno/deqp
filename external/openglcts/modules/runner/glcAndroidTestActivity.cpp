33b4d88c8eddb7621e258758045550966ecb38e5
Connor Abbott <cwabbott0@gmail.com>
2020-03-11 16:07:07 +0100

    HACK: build executables on Android
    cmake .. -GNinja -DDEQP_TARGET=android
    -DDEQP_TARGET_TOOLCHAIN=ndk-modern -DANDROID_ABI=arm64-v8a
    -DANDROID_NDK_PATH=$HOME/android-ndk-r21d -DCMAKE_BUILD_TYPE=Release
    -DDE_ANDROID_API=28 -DGLCTS_GTF_TARGET=gles31
    
    Change-Id: Ib72409ed56a8bc2c720230d6e000ced6f76d3e27

--- external/openglcts/modules/runner/glcAndroidTestActivity.cpp
+++ external/openglcts/modules/runner/glcAndroidTestActivity.cpp
@@ -66,7 +66,7 @@ static deUint32 getFlags(ANativeActivity* activity)
 TestThread::TestThread(NativeActivity& activity, tcu::Android::AssetArchive& archive, const std::string& waiverPath,
 					   const std::string& logPath, glu::ApiType runType, deUint32 runFlags)
 	: RenderThread(activity)
-	, m_platform(activity)
+	, m_platform()
 	, m_archive(archive)
 	, m_app(m_platform, m_archive, waiverPath.c_str(), logPath.c_str(), runType, runFlags)
 	, m_finished(false)
--- framework/platform/android/tcuAndroidPlatform.cpp
+++ framework/platform/android/tcuAndroidPlatform.cpp
@@ -280,33 +280,20 @@ private:
 	WindowRegistry&		m_windowRegistry;
 };
 
-static size_t getTotalSystemMemory (ANativeActivity* activity)
+static size_t getTotalSystemMemory (void)
 {
 	const size_t	MiB		= (size_t)(1<<20);
 
-	try
-	{
-		const size_t totalMemory = getTotalAndroidSystemMemory(activity);
-		print("Device has %.2f MiB of system memory\n", static_cast<double>(totalMemory) / static_cast<double>(MiB));
-		return totalMemory;
-	}
-	catch (const std::exception& e)
-	{
-		// Use relatively high fallback size to encourage CDD-compliant behavior
-		const size_t	fallbackSize	= (sizeof(void*) == sizeof(deUint64)) ? 2048*MiB : 1024*MiB;
+	// Use relatively high fallback size to encourage CDD-compliant behavior
+	const size_t	fallbackSize	= (sizeof(void*) == sizeof(deUint64)) ? 2048*MiB : 1024*MiB;
 
-		print("WARNING: Failed to determine system memory size required by CDD: %s\n", e.what());
-		print("WARNING: Using fall-back size of %.2f MiB\n", double(fallbackSize) / double(MiB));
-
-		return fallbackSize;
-	}
+	return fallbackSize;
 }
 
 // Platform
 
-Platform::Platform (NativeActivity& activity)
-	: m_activity			(activity)
-	, m_totalSystemMemory	(getTotalSystemMemory(activity.getNativeActivity()))
+Platform::Platform (void)
+	: m_totalSystemMemory	(getTotalSystemMemory())
 {
 	m_nativeDisplayFactoryRegistry.registerFactory(new NativeDisplayFactory(m_windowRegistry));
 	m_contextFactoryRegistry.registerFactory(new eglu::GLContextFactory(m_nativeDisplayFactoryRegistry));
@@ -329,7 +316,7 @@ vk::Library* Platform::createLibrary (void) const
 
 void Platform::describePlatform (std::ostream& dst) const
 {
-	tcu::Android::describePlatform(m_activity.getNativeActivity(), dst);
+	dst << "unknown";
 }
 
 void Platform::getMemoryLimits (vk::PlatformMemoryLimits& limits) const
@@ -369,3 +356,8 @@ bool Platform::hasDisplay (vk::wsi::Type wsiType) const
 
 } // Android
 } // tcu
+
+tcu::Platform* createPlatform (void)
+{
+	return new tcu::Android::Platform();
+}
